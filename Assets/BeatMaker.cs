7f852d8073775dd828929e9e09f8d1ea00c23372
ru1t3rl <lucaruiters@hotmail.nl>
2020-11-14 22:14:25 +0100

    Started working on the beat maker

---
+++ Assets/BeatMaker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrackTheMusic.Audio.Creator {
+public class BeatMaker : MonoBehaviour
+{
+    
+}
+
+public class Track {
+    Instrument instrument;
+}
+}
